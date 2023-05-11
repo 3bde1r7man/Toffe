04013975521dd594fbf8dbacd32d0b14779059b0
Ahmed Hanfy <ahanfybekheet@gmail.com>
2023-05-11 18:45:28 +0200

    take parameters of login , register function as inputs inside it

--- src/main/java/com/mycompany/toffe/Toffe.java
+++ src/main/java/com/mycompany/toffe/Toffe.java
@@ -6,9 +6,9 @@ package com.mycompany.toffe;
 
 public class Toffe {
     public static void main(String[] args) {
-        // User user = new User();
-        // user.register("ahmedHanfy", "ahanfybekheet@gmail.com", "Ahmed@064", "asd");
-        // user.login("ahmedHanfy", "Ahmed@064");
+        User user = new User();
+        user.register();
+        user.login();
         System.out.println("Hello World!");
     }
 }
